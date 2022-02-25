066e39322e0b0c3e09b6679776f6dff82859e688
Gorka7 <72950060+Gorka7@users.noreply.github.com>
2022-02-25 22:07:47 +0100

    Add files via upload

---
+++ 5-Scanner/src/scanner/Main.java
@@ -0,0 +1,20 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner (System.in);
+		System.out.println("Introduce tu nombre");
+		String nombre = teclado.nextLine();
+		System.out.println("Bienvenid@ "+nombre);
+		teclado.close();
+		
+		
+	}
+	
+	
+
+}
