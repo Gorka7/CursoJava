01226c6173c2c0e1bbcc0bc9b712ba5033816306
Gorkii <72950060+Gorka7@users.noreply.github.com>
2022-03-07 16:08:50 +0100

    Add files via upload

---
+++ PilaresPoo2/src/pilaresPOO2/ClienteVIP.java
@@ -0,0 +1,15 @@
+package pilaresPOO2;
+
+public class ClienteVIP extends Cliente{
+	
+	
+	
+	public int comprar(int saldo) {
+		saldo=4000;
+		//System.out.println("Estoy comprando como un PRO "+", con Saldo de "+saldo+"€");
+		System.out.println(saldo);
+		return saldo;
+	}
+	
+
+}
---
+++ PilaresPoo2/src/pilaresPOO2/Empleado.java
@@ -0,0 +1,9 @@
+package pilaresPOO2;
+
+import persona2.Persona;
+
+public class Empleado extends Persona{
+
+	String numCuenta;
+	String nss;
+}
